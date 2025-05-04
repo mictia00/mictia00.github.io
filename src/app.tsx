70e56e1669a8b8b270c97f4cafeef9c927c0a752
mictia00 <star0fym@gmail.com>
2025-05-03 21:10:26 -0300

    feat: add global navbar component

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import Router, { Route } from 'preact-router';
+import { Navbar } from '@/components/Navbar';
 import { Home } from '@/routes/Home';
 import { NotFound404 } from '@/routes/404';
 
@@ -8,7 +9,7 @@ import injectContext from '@/store/appContext';
 function App() {
     return (
         <>
-            {/* NavBar */}
+            <Navbar />
             <Router>
                 <Route component={Home} path='/' />
                 <Route component={NotFound404} default />
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,43 @@
+import { useState } from "preact/hooks";
+import { route } from "preact-router";
+
+import Logo from "@/assets/mictiWoa.png";
+import Menu from "@/assets/burger.png";
+import CloseMenu from "@/assets/burger_x.png";
+import "./navbar.css";
+
+
+const navbarContent: { text: string, path: string }[] = [
+    // Example: { text: 'Home', path: '/' },
+];
+
+export function Navbar() {
+    const [viewOverlay, setViewOverlay] = useState(false);
+
+    const toggleViewOverlay = () => setViewOverlay(!viewOverlay);
+    const clickLink = (path: string) => () => { viewOverlay && toggleViewOverlay(); route(path); };
+    const ulContent = <ul>{navbarContent.map((nc) => <li onClick={clickLink(nc.path)}>{nc.text}</li>)}</ul>;
+    
+    return (
+        <>
+            {/* Navbar principal */}
+            <nav id="navbar">
+                <div class="brand">
+                    <img src={Logo} alt="Logo" />
+                    <h1 onClick={clickLink('/')}>@Mictia00</h1>
+                </div>
+                {ulContent}
+                <img onClick={toggleViewOverlay} src={viewOverlay ? CloseMenu : Menu} alt="Menú" class="toggle-icon" id="toggleIcon" />
+                {/* Overlay para el menú en dispositivos móviles */}
+                <div className={`overlay ${viewOverlay ? 'active': ''}`}>
+                    <div class="brand">
+                        <img src={Logo} alt="Logo" />
+                        <h1 onClick={clickLink('/')}>@Mictia00</h1>
+                    </div>
+                    <h2>Streamer ・ Creadora de contenido ・ Ilustradora</h2>
+                    {ulContent}
+                </div>
+            </nav>
+        </>
+    );
+}
